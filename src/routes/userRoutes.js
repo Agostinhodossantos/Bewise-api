3a5309a9ff030a4d7dde7886665df514a29807f3
Agostinhodossantos <agostinhodossantos27@gmail.com>
2021-11-19 15:29:54 +0200

    add adminsdk

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 const authController = require('./../controllers/authController');
 
-
 router.post('/login', authController.login);
 router.post('/signup', authController.signup);
 
@@ -11,4 +10,5 @@ router.get('/users', userController.getAllUsers);
 router.get('/', (req, res) => {
     res.send({name: "hello world"})
 })
+
 module.exports = router;
